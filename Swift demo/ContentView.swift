85db532c20547b42dda08770395a1e642e02c474
Cotemustis <jose.i.villablanca@gmail.com>
2021-05-24 19:05:03 -0400

    Initial Commit

---
+++ Swift demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift demo
+//
+//  Created by Cotemustis on 24-05-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift demo/Swift_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_demoApp.swift
+//  Swift demo
+//
+//  Created by Cotemustis on 24-05-21.
+//
+
+import SwiftUI
+
+@main
+struct Swift_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
