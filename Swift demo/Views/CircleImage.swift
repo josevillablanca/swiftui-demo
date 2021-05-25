6dfe7b2b356da7bfb90f352f381af752728c0255
Cotemustis <jose.i.villablanca@gmail.com>
2021-05-24 22:52:52 -0400

    Navigation is working with the integration of the links

--- Swift demo/Views/CircleImage.swift
+++ Swift demo/Views/CircleImage.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 
 struct CircleImage: View {
+    var image: Image
+    
     var body: some View {
-        Image("turtlerock")
+        image
             .frame(width: 200, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
             .clipShape(Circle())
             .overlay(Circle().stroke(Color.white, lineWidth: 4))
@@ -19,6 +21,6 @@ struct CircleImage: View {
 
 struct CircleImage_Previews: PreviewProvider {
     static var previews: some View {
-        CircleImage()
+        CircleImage(image: Image("turtlerock"))
     }
 }
