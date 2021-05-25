0b95156fc91e252a6d61c263d66bd2017c320424
Cotemustis <jose.i.villablanca@gmail.com>
2021-05-24 20:06:28 -0400

    Added gitignore and first step of main tutorial

---
+++ Swift demo/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  Swift demo
+//
+//  Created by Cotemustis on 24-05-21.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtle-rock")
+            .frame(width: 200, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
